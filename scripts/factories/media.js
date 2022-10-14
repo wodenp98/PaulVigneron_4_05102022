d55f191437a251cc150e5da4029264bc806f1eea
Paul VIGNERON <paulvigneron4698@gmail.Com>
2022-10-14 16:33:54 +0200

    Css article photograph

--- scripts/factories/media.js
+++ scripts/factories/media.js
@@ -4,16 +4,20 @@ function mediaFactory(data) {
     function getMediaCardDOM() {
         const article = document.createElement("article");
 
-        const imageMedia = `<img src="/assets/images/${image}" alt="${title}" />`
-        const videoMedia = `<img src="/assets/images/${video}" alt="${title}" />`
+        const imageMedia = `<img src="/assets/images/${image}" alt="${title}" class="article-image" />`
+        const videoMedia = `<video controls src="/assets/images/${video}" alt="${title}" class="article-image" />`
 
 
-        article.innerHTML = `<div>
+        article.innerHTML = `<a aria-label="${title}" class="article-link">
                                 ${image ? imageMedia : videoMedia}
-                                <p class="image-title">${title}</p>
-                                <span class="likes-number" aria-label="likes">${likes}</span>
+                             </a>
+                             <div class="article-infos">
+                                <p class="article-title">${title}</p>
+                                <span class="article-likes" aria-label="likes">${likes}<i class="article-icon fa-sharp fa-solid fa-heart"></i></span>
                             </div>
                             `;
+        article.classList.add("article-portfolio")
+        
         return article;
     }
     return { date, id, image, video, likes, photographerId, price, title, getMediaCardDOM }
