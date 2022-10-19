d8e421e87cd7df8899188cb298acbd80f818c8ca
Paul VIGNERON <paulvigneron4698@gmail.Com>
2022-10-19 03:27:12 +0200

    alt property and title for lightbox

--- scripts/utils/lightbox.js
+++ scripts/utils/lightbox.js
@@ -6,14 +6,16 @@ const right = document.querySelector(".lightbox-right");
 
 lightbox.style.display = "none";
 
-function lightboxDisplay(arg) {
+function lightboxDisplay(arg, arg2) {
   let media = arg.split(".");
   media = media[media.length - 1];
 
   if (media === "jpg") {
-    lightboxImg.innerHTML = `<img src="${arg}" alt="${arg}" class="image-lightbox"/>`;
+    lightboxImg.innerHTML = `<img src="${arg}" alt="${arg2}" class="image-lightbox"/>`;
+    lightboxImg.innerHTML += `<p>${arg2}</p>`;
   } else {
-    lightboxImg.innerHTML = `<video controls src="${arg}" alt="" class="image-lightbox" />`;
+    lightboxImg.innerHTML = `<video controls src="${arg}" alt="${arg2}" class="image-lightbox" />`;
+    lightboxImg.innerHTML += `<p>${arg2}</p>`;
   }
 }
 
@@ -22,7 +24,7 @@ function lightboxOpen() {
   images.forEach((image) =>
     image.addEventListener("click", (e) => {
       e.preventDefault();
-      lightboxDisplay(image.src);
+      lightboxDisplay(image.src, image.alt);
       lightbox.style.display = "block";
       header.style.display = "none";
       main.style.display = "none";
@@ -51,9 +53,10 @@ function lightboxNext() {
     if (indexImg === gallery.length - 1) {
       indexImg = -1;
     }
-    const nextImageAlt = images[indexImg + 1].src;
+    const nextImageAlt = images[indexImg + 1].getAttribute("alt");
+    const nextImageSrc = images[indexImg + 1].src;
 
-    lightboxDisplay(nextImageAlt);
+    lightboxDisplay(nextImageSrc, nextImageAlt);
   });
 }
 
@@ -67,11 +70,13 @@ function lightboxPrev() {
     let indexImg = gallery.findIndex((img) => img === currentImg.currentSrc);
 
     if (indexImg === 0) {
-      const prevImageAlt = images[images.length - 1].src;
-      lightboxDisplay(prevImageAlt);
+      const prevImageSrc = images[images.length - 1].src;
+      const prevImageAlt = images[images.length - 1].getAttribute("alt");
+      lightboxDisplay(prevImageSrc, prevImageAlt);
     } else {
-      const prevImageAlt = images[indexImg - 1].src;
-      lightboxDisplay(prevImageAlt);
+      const prevImageSrc = images[indexImg - 1].src;
+      const prevImageAlt = images[indexImg - 1].getAttribute("alt");
+      lightboxDisplay(prevImageSrc, prevImageAlt);
     }
   });
 }
